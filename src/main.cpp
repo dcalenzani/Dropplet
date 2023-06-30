238485249a1bb8e0651edf03855fef024b509efe
dcalenzani <dcalenzani@pucp.pe>
2023-06-29 19:22:44 -0500

    First big changes on the main code. Sensors do work and FreeRTOS implementation on WiFi works, but together they produce memory errors. Shall fix those for next commit as well as the database connection

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 OBJECTIVE: Program the ESP32 microcontroller to use peripherals to measure humidity conditions and water plants accordingly, passing data to a web API.
 
-The organization of the code puts moduluratity first, systems pin definitions and its functions will be together instead of definition > function structure. Stakeholder testing for ESP32 still undone. Future add-ons projected: Motor pump, Wifi Server instead of client, Battery and battery life indicator on ESP32
+The organization of the code puts moduluratity first, systems pin definitions and its functions will be together. Stakeholder testing for ESP32 still undone. Future add-ons projected: Motor pump, Wifi Server instead of client, Battery and battery life indicator on ESP32
 */
 
 // Librarys used for the project. I always follow the policy of using the least amount of libraries possible.
@@ -28,8 +28,8 @@ hw_timer_t *timer_1 = NULL; // Timer 1 millis
 void IRAM_ATTR RGBLeds() {
   redLedState = !redLedState;
   greenLedState = !greenLedState;
-  digitalWrite(GreenLed, redLedState);
-  digitalWrite(RedLed, greenLedState);
+  digitalWrite(GreenLed, greenLedState);
+  digitalWrite(RedLed, redLedState);
 }
 
 /* ANALOG SENSORS
@@ -44,40 +44,16 @@ hw_timer_t *timer_2 = NULL; // Timer 2 millis
 
 // Resistive soil sensors are (as their name states) based on resistance, so they are read as any analog value.
 void humRead(){
-
+  unsigned long previousMillis = 0;
+  unsigned long currentMillis = millis(); // Get the current time
+  hum_dht = analogRead(dht11);
+  Serial.print("DHT value: ");
+  Serial.println(hum_dht);
+  hum_soil = analogRead(SoilSensor);
+  Serial.print("RESISTIVE SOIL value: ");
+  Serial.println(hum_soil);
 }
 
-void humSend(){
-
-}
-
-/* WIFI CONNECTION
-The blue led its for communicating phisically the state. A blink each second means that the Wifi its OK, if the Led stays on then it means that the Wifi is disconnected. Fast Blinking (500ms) means it has disconnected and manual connection is required 
-*/
-#define BlueLed 27
-volatile bool blueLedState = false;
-
-const char* ssid = "DANIEL_plus";
-const char* password = "10552669";
-const char* serverName = "http://192.168.1.54/api/dropplet";
-
-void APHandler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
-  Serial.println("Connected To The WiFi Network");
-  
-}
- 
-void IPHandler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
-  Serial.print("Local ESP32 IP: ");
-  Serial.println(WiFi.localIP());
-}
- 
-void DisconnectionHandler(WiFiEvent_t wifi_event, WiFiEventInfo_t wifi_info) {
-  Serial.println("Disconnected From WiFi Network");
-  // Attempt Re-Connection
-  WiFi.begin(ssid, password);
-}
-
-/*
 void humPost(){
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
@@ -85,16 +61,65 @@ void humPost(){
     http.addHeader("Content-Type", "application/json");
     String payload = "{\"humidity\":2.22,\"peristatus\":true}";
     int httpCode = http.POST(payload);
-    String response = http.getString();
+    static String response = http.getString();
     Serial.println(response);
     http.end();
   }
 }
+
+/* WIFI CONNECTION
+The blue led its for communicating phisically the state. A blink each second means that the Wifi its OK, if the Led stays on then it means that the Wifi is disconnected. Fast Blinking (500ms) means it has disconnected and manual connection is required 
 */
+
+#define BlueLed 27
+volatile bool blueLedState = false;
+
+#define WIFI_NETWORK "DANIEL"
+#define WIFI_PASSWORD "10552669"
+#define WIFI_TIMEOUT_MS 20000
+
+void keepWiFiAlive(void * parameters){
+  for(;;){
+    if(WiFi.status() == WL_CONNECTED){
+      Serial.println("WiFi still connected");
+      vTaskDelay(10000 / portTICK_PERIOD_MS);
+      digitalWrite(BlueLed, HIGH);
+      continue;
+    }
+
+    Serial.println("WiFi Connecting");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
+    unsigned long startAttemptTime = millis();
+
+    // Keep looping while we're not connected and haven't reached the timeout
+    while (WiFi.status() != WL_CONNECTED &&
+          millis() - startAttemptTime < WIFI_TIMEOUT_MS){}
+    // When we couldn't make a WiFi connection
+      if(WiFi.status() != WL_CONNECTED){
+        Serial.println("[WIFI] FAILED");
+        vTaskDelay(20000/ portTICK_PERIOD_MS);
+        continue;
+      }
+
+    Serial.println("[WIFI] Connected: " + WiFi.localIP());
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
+  xTaskCreatePinnedToCore(
+    keepWiFiAlive,
+    "KEEP WIFI ALIVE",
+    5000,
+    NULL,
+    1,
+    NULL,
+    CONFIG_ARDUINO_RUNNING_CORE
+  );
+  
   pinMode(RedLed, OUTPUT);
   pinMode(GreenLed, OUTPUT);
   pinMode(BlueLed, OUTPUT);
@@ -103,46 +128,14 @@ void setup() {
   timerAttachInterrupt(timer_1, &RGBLeds, true);
   timerAlarmWrite(timer_1, 1000000, true); // 1000000 microseconds = 1 second
   timerAlarmEnable(timer_1);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.onEvent(APHandler, ARDUINO_EVENT_WIFI_STA_CONNECTED);
-  WiFi.onEvent(IPHandler, ARDUINO_EVENT_WIFI_STA_GOT_IP);
-  WiFi.onEvent(DisconnectionHandler, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting to WiFi Network ..");
-
 }
 
-unsigned long previousMillis = 0;
-
 void loop() {
+  unsigned long previousMillis = 0;
   unsigned long currentMillis = millis(); // Get the current time
-  if (currentMillis - previousMillis < 2000) {
-  hum_dht = analogRead(dht11);
-  Serial.print("DHT value: ");
-  Serial.println(hum_dht);
-  hum_dht = analogRead(SoilSensor);
-  Serial.print("RESISTIVE SOIL value: ");
-  Serial.println(hum_soil);
+  if (currentMillis - previousMillis < 200000) {
+    humRead();
+    humPost();
   previousMillis = currentMillis;
   };
-  /*
-  static unsigned long lastRequestTime = 0;
-  const unsigned long requestInterval = 10000000; // milliseconds
-  
-  // put your main code here, to run repeatedly:
-  if (WiFi.status() == WL_CONNECTED) {
-    if (millis() - lastRequestTime >= requestInterval) {
-      lastRequestTime = millis();
-      
-      HTTPClient http;
-      http.begin("http://192.168.1.34:3000/api/dropplet");
-      http.addHeader("Content-Type", "application/json");
-      int httpResponseCode = http.POST("{\"humidity\":2.22,\"peristatus\":true}");
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      http.end();
-    }
-  }
-  */
 }
